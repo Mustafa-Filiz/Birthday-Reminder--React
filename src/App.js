5ab632bf2125a9f6817f4e946a341f61a825da55
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-09-16 16:01:09 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
+    const [showItem, setShowItem] = useState(true);
 
-  const [showItem, setShowItem] = useState(true)
-
-  return (
-      <main>
-        <section className="container">
-          <h3>{showItem ? data.length : "0"} birthdays today</h3>
-          {showItem && data.map((item) => <List person={item} />)}
-          <button onClick={() => setShowItem(!showItem)}>{showItem ? "Clear All" : "View All"}</button>
-        </section>
-      </main>
-  );
+    return (
+        <main>
+            <section className="container">
+                <h3>{showItem ? data.length : '0'} birthdays today</h3>
+                {showItem && data.map((item) => <List person={item} />)}
+                <button onClick={() => setShowItem(!showItem)}>
+                    {showItem ? 'Clear All' : 'View All'}
+                </button>
+            </section>
+        </main>
+    );
 }
 
 export default App;
